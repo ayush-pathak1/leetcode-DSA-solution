71dec321750fa92d677a37641a2d7b7c35d87598
ayush-pathak1 <115356868+ayush-pathak1@users.noreply.github.com>
2026-03-03 18:50:00 +0530

    Time: 11 ms (74.16%) | Memory: 12.4 MB (86.58%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        current_row = 0
+        step = 1
+
+        for char in s:
+            rows[current_row] += char
+            
+            if current_row == 0:
+                step = 1
+            elif current_row == numRows - 1:
+                step = -1
+            
+            current_row += step
+
+        return "".join(rows)
