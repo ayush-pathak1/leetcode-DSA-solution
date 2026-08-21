c921a8c83fafc5c55585a2ddd8768f8b696f1a22
ayush-pathak1 <115356868+ayush-pathak1@users.noreply.github.com>
2026-08-21 23:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (81.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -9,7 +9,6 @@ class Solution(object):
         
         if m == 0:
             return 0
-        
         for i in range(n - m + 1):
             if haystack[i:i + m] == needle:
                 return i
